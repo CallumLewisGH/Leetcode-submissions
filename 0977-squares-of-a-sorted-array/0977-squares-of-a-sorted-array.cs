a917d4eb19664d4f768bffeebb1c493dd73f982c
Callum Lewis <callumlewis852@gmail.com>
2024-12-18 22:21:30 +0000

    Time: 1 ms (100%), Space: 64.1 MB (30.36%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cs
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cs
@@ -1,12 +1,31 @@
 public class Solution {
-    public int[] SortedSquares(int[] nums) 
-    {
-        for (int i = 0; i < nums.Length; i++) 
-        {
-            nums[i] = nums[i] * nums[i];
-        }
-        Array.Sort(nums);
+   public int[] SortedSquares(int[] nums)
+{
+    int pointer1 = 0; 
+    int pointer2 = nums.Length - 1;
+    int[] result = new int[nums.Length]; 
+    int insertPos = nums.Length - 1; 
 
-        return nums;
+    while (pointer1 <= pointer2)
+    {
+        int leftSquare = nums[pointer1] * nums[pointer1];
+        int rightSquare = nums[pointer2] * nums[pointer2];
+
+        if (leftSquare > rightSquare)
+        {
+            result[insertPos] = leftSquare;
+            pointer1 += 1;
+        }
+        else
+        {
+            result[insertPos] = rightSquare;
+            pointer2 -= 1;
+        }
+
+        insertPos -= 1;
     }
+
+    return result;
+}
+
 }
